07a4a928d2614501b11124084ec5c4e544d250a8
YoheiFukudome <pt83s0502@gmail.com>
2024-05-30 19:25:26 +0900

    page-service.phpのjsファイルをfunctions.phpへ移行

--- wp-content/themes/fujiwara/page-service.php
+++ wp-content/themes/fujiwara/page-service.php
@@ -146,18 +146,3 @@ Template Name: Service page
 <!-- /main -->
 <!-- footer呼び出し -->
 <?php get_footer(); ?>
-<!-- gsap.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/gsap.min.js"></script>
-<!-- ScrollTrigger.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/ScrollTrigger.min.js"></script>
-<!-- common.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.bundle.js"></script>
-<!-- service.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/service.bundle.js"></script>
-<!-- IE object-fit -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.js"></script>
-<script>
-objectFitImages();
-</script>
-<!-- polyfill(picture IE) -->
-<script src="https://polyfill.io/v3/polyfill.min.js?features=HTMLPictureElement"></script>
