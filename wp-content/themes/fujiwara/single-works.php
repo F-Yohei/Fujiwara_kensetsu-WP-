5ef6742f5f53099217685f6fd2223e7f1026a793
YoheiFukudome <pt83s0502@gmail.com>
2024-05-30 22:22:14 +0900

    single-works.phpのjsファイルをfunctions.phpへ移行

--- wp-content/themes/fujiwara/single-works.php
+++ wp-content/themes/fujiwara/single-works.php
@@ -211,32 +211,3 @@ Template Name: Works detail page
 <!-- /main -->
 <!-- footer呼び出し -->
 <?php get_footer(); ?>
-<!-- TweenMax.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/TweenMax.min.js"></script>
-<!-- worksCursor.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/worksCursor.js"></script>
-<!-- gsap.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/gsap.min.js"></script>
-<!-- ScrollTrigger.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/ScrollTrigger.min.js"></script>
-<!-- glightbox.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/glightbox.min.js"></script>
-<!-- common.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.bundle.js"></script>
-<!-- worksdetail.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/worksdetail.bundle.js"></script>
-<!-- IE object-fit -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.js"></script>
-<script>
-objectFitImages();
-</script>
-<!-- polyfill(picture IE) -->
-<script src="https://polyfill.io/v3/polyfill.min.js?features=HTMLPictureElement"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.6.0.min.js"></script>
-<!-- glightbox.min.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript">
-</script>
-</div>
-</body>
-
-</html>
