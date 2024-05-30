fe6fe4ae6aac87cf1e92947b1ddc201eaf67a597
YoheiFukudome <pt83s0502@gmail.com>
2024-05-31 08:30:23 +0900

    archive-works.phpのjsファイルをfunctions.phpへ移行

--- wp-content/themes/fujiwara/archive-works.php
+++ wp-content/themes/fujiwara/archive-works.php
@@ -139,22 +139,3 @@ Template Name: Works Category page
 <!-- /main -->
 <!-- footer呼び出し -->
 <?php get_footer(); ?>
-<!-- gsap.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/gsap.min.js"></script>
-<!-- ScrollTrigger.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/ScrollTrigger.min.js"></script>
-<!-- common.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.bundle.js"></script>
-<!-- works.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/works.bundle.js"></script>
-<!-- IE object-fit -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.js"></script>
-<script>
-objectFitImages();
-</script>
-<!-- polyfill(picture IE) -->
-<script src="https://polyfill.io/v3/polyfill.min.js?features=HTMLPictureElement"></script>
-</div>
-</body>
-
-</html>
