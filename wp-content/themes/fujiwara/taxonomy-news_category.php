e60430c65b65ef63ae6b5d1e84634c7d148a08a9
YoheiFukudome <pt83s0502@gmail.com>
2024-05-31 19:43:24 +0900

    taxonomy-news_category.phpのjsファイルをfunctions.phpへ移行

--- wp-content/themes/fujiwara/taxonomy-news_category.php
+++ wp-content/themes/fujiwara/taxonomy-news_category.php
@@ -153,22 +153,3 @@ Template Name: News page
 <!-- /main -->
 <!-- footer呼び出し -->
 <?php get_footer(); ?>
-<!-- gsap.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/gsap.min.js"></script>
-<!-- ScrollTrigger.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/ScrollTrigger.min.js"></script>
-<!-- glightbox.min.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/glightbox.min.js"></script>
-<!-- common.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.bundle.js"></script>
-<!-- IE object-fit -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.js"></script>
-<script>
-objectFitImages();
-</script>
-<!-- polyfill(picture IE) -->
-<script src="https://polyfill.io/v3/polyfill.min.js?features=HTMLPictureElement"></script>
-<!-- clipboard.min.js -->
-<script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
-<!-- news.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/news.bundle.js"></script>
